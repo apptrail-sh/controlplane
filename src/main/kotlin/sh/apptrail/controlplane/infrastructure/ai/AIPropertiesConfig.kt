73549a8d289db3b1ebc6112daa0276b9103e4fc1
Fernando Nogueira <fernandonogueira@users.noreply.github.com>
2026-01-22 19:03:06 -0300

    fix(config): remove unused AI configurations and dependencies

--- src/main/kotlin/sh/apptrail/controlplane/infrastructure/ai/AIPropertiesConfig.kt
+++
@@ -1,9 +0,0 @@
-package sh.apptrail.controlplane.infrastructure.ai
-
-import org.springframework.boot.context.properties.EnableConfigurationProperties
-import org.springframework.context.annotation.Configuration
-import sh.apptrail.controlplane.infrastructure.ai.openai.AIProperties
-
-@Configuration
-@EnableConfigurationProperties(AIProperties::class)
-class AIPropertiesConfig
